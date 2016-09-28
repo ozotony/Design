400c4942f457085f7fcc0ca486f968ca1ea49e5a
unknown <ozotony@yahoo.com>
2016-09-28 12:57:56 +0100

    latest2

--- js/AngularLogin.js
+++ js/AngularLogin.js
@@ -1,7 +1,7 @@
 ﻿var app = angular.module('myModule', ['smart-table', 'angular-loading-bar']);
-//var serviceBaseDs = 'http://ds.cldng.com/';
+var serviceBaseDs = 'http://ds.cldng.com/';
 
-var serviceBaseDs = 'http://localhost:60693/';
+
 
 
 
